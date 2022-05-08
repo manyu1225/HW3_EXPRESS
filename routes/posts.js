1e5573f4f629d10990cc9a523338f177420f1303
manyu1225 <manyu1225@gmail.com>
2022-05-08 19:46:46 +0800

    5/4每日任務Day19

--- routes/posts.js
+++ routes/posts.js
@@ -2,13 +2,23 @@ var express = require('express');
 var router = express.Router();
 const Post = require('../models/post');
  
+//         在 GET posts 網址列帶入排序及限制筆數的參數
+// 並運用 Express 提供的 req.query 取得網址列的參數，將尋找到符合的資料設定排序及呈現指定資料數量
+
 /* GET users listing. */
 router.get('/', async (req, res, next) =>  {
-    const allUser  =await Post.find();
+  const timeSort = req.query.timeSort === 'asc' ? 'createdAt' : '-createdAt';
+  const limit = req.query.limit;
+  console.log(timeSort);
+  console.log(limit);
+    const all  =await Post.find().sort(timeSort).limit(limit);
     res.status(200).json({
       "status": 'success',
-      "data": allUser
+      data: {
+        all
+      }
     });
+
   });
 
 
@@ -42,20 +52,25 @@ router.get('/', async (req, res, next) =>  {
 
   router.post('/', async (req, res, next) =>{
     try{
-      const data = req.body;
+      const data = req.body;  // 取得來自 request body 的資料
       let { name, tags, type,content} = data;
-  
-      if (!name||!type ) {
+   // 驗證是否有 content 欄位 -> 若有則使用 mongoose 語法新增資料 -> 回傳成功回應
+      //                       -> 未填寫 content 欄位 -> 回傳失敗
+      if (!name||!type ||!content) {
         // 回傳失敗
         res.status(400).json({
           "status": 'fail',
           "message": '請確認欄位'
         })
         return;
-      } 
-      const  newUser= await Post.create({ name, tags, type,content})
-      res.status(200).json({ status: 'success', newUser });  
-   
+      }else{
+
+        const  newUser= await Post.create({ name, tags, type,content})
+        res.status(200).json({ status: 'success', newUser });  
+
+
+      }
+
     } catch (error) {
       // 回傳失敗
       res.status(400).json({
